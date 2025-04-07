4a49b674c44c951cfcf5b9418bcaa3119d14bd80
Aseran20 <adriturion.com>
2025-04-07 23:43:30 +0200

    Fix Supabase URL

--- js/supabase.js
+++ js/supabase.js
@@ -1,5 +1,5 @@
 // Initialize Supabase client
-const supabaseUrl = 'https://kqnylrieuxhprqehbsz.supabase.co'
+const supabaseUrl = 'https://kqnylriexuhpryqebhsz.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtxbnlscmlleHVocHJ5cWViaHN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQwNTc4NjksImV4cCI6MjA1OTYzMzg2OX0.YfaUPRQxtPtpKeazYFi9oNTBlmb46rDnvbPUN4UMqDw' // Replace with your actual anon key
 
 export const supabase = window.supabase.createClient(supabaseUrl, supabaseKey)
